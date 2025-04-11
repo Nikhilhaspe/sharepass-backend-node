d3042119bb7882602edbfde549e5a3aca5ab790a
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2025-04-11 23:33:55 +0530

    handle uncaught excpeptions, uncaught rejections

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception 💥");
+  console.error(`${err.name} : ${err.message}`);
+
+  // kill the server
+  process.exit(1);
+});
+
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
@@ -14,4 +22,20 @@ mongoose.connect(DB).then((connection) => console.log("Connected To DB ✅"));
 const app = require("./app");
 const SERVER = app.listen(process.env.PORT || 3000, () => {
   console.log(`App listening on port ${process.env.port || 3000} ✅`);
+});
+
+// ERRORS: outisde express app
+
+// globally catch unhandled rejctions
+// handling unhandled rejection
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection 💥");
+  console.error(`${err.name}${err.message}`);
+
+  // gracefull shutdown
+  SERVER.close(() => {
+    console.log("Finishing ongoing requests...");
+    console.log("Shutting down...");
+    process.exit(1);
+  });
 });
